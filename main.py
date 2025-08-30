8965204e78fe11925563ed17b56f1716251da52e
RICHQAQ <2206144600@qq.com>
2025-08-30 15:36:21 +0800

    Initial commit: version 0.1

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+__version__ = "0.1.0"  # 添加版本号
 import os, sys, json, tempfile, subprocess, pathlib, time, threading, io, traceback
 from datetime import datetime
 from typing import Optional
